65367f59c852e73daf4b7eb41187450980409b60
Felipe Augusto <felipeaugustolopesveras@gmail.com>
2022-06-14 20:30:31 -0300

    Add files via upload

--- lista_1B/p9.c
+++ lista_1B/p9.c
@@ -1,129 +1,125 @@
 #include <stdio.h>
 #include <math.h>
 
-/*char *plural(int m, int c, int d, int u)
-{
+int main(){
+    int n, m, c, d, u, r1000, r100, r10, ordem;
+    ordem=0;
+    scanf("%d", &n);
 
-  char *Ms = "unidade";
-  char *Cs = "centena";
-  char *Ds = "dezena";
-  char *Us = "unidade";
+    r1000 = n % 1000;
+    r100 = n % 100;
+    r10 = n % 10;
 
-  if (m > 1)
-  {
-    Ms = "unidades";
-  }
+    m = (n - r1000) / 1000;
+    c = (r1000 - r100) / 100;
+    d = (r100 - r10) / 10;
+    u = r10;
 
-  if (c > 1)
-  {
-    Cs = "centenas";
-  }
+    if (n <= 0 || m >= 10)
+    {
+        printf("Numero invalido!\n");
+    }
+    else
+    {
+        if (m > 0)
+        {
+            ordem=4;
+            printf("(quarta ordem) %d = ", n);
+            if(m > 1){
+                printf("%d unidades de milhar ",m);
+            }
+            else{
+                printf("%d unidade de milhar ",m);
+            }
+        }
+        if (c>0)
+        {
+            if(ordem==0){
+                ordem=3;
+                printf("(terceira ordem) %d = ", n);
+            }
+            else
+            {
+                printf("+ ");
+            }
+            
+            if(c>1){
+                printf("%d centenas ",c);
+            }
+            else{
+                printf("%d centena ",c);
+            }
+        }
+        if (d > 0)
+            {
+            if(ordem==0){
+                ordem=2;
+                printf("(segunda ordem) %d = ", n);
+            }
+            else
+            {
+                printf("+ ");
+            }
+            if(d>1){
+                printf("%d dezenas ",d);
+            }
+            else{
+                printf("%d dezena ",d);
+            }
+            }
+        if (u > 0)
+        {
+            if(ordem==0){
+                ordem=1;
+                printf("(primeira ordem) %d = ", n);
+            }
+            else
+            {
+                printf("+ ");
+            }
+            if(u>1){
+                printf("%d unidades ",u);
+            }
+            else{
+                printf("%d unidade ",u);
+            }
+        }
 
-  if (d > 1)
-  {
-    Ds = "dezenas";
-  }
-
-  if (u > 1)
-  {
-    Us = "unidades";
-  }
-
-  return Ms, Cs, Ds, Us;
-}*/
-
-/*char *ordem(int m, int c, int d, int u)
-{
-  char *o;
-
-  if (m > 0)
-  {
-    o = "quarta ordem";
-  }
-  else if (m == 0 && c > 0)
-  {
-    o = "terceira ordem";
-  }
-  else if (m == 0 && c == 0 && d > 0)
-  {
-    o = "segunda ordem";
-  }
-  else if (m == 0 && c == 0 && d == 0 && u > 0)
-  {
-    o = "primeira ordem";
-  }
-
-  return o;
-}*/
-
-int main()
-{
-  int n, m, c, d, u, r1000, r100, r10;
-  scanf("%d", &n);
-
-  r1000 = n % 1000;
-  r100 = n % 100;
-  r10 = n % 10;
-
-  m = (n - r1000) / 1000;
-  c = (r1000 - r100) / 100;
-  d = (r100 - r10) / 10;
-  u = r10;
-
-  if (n == 0 || m >= 10)
-  {
-    printf("Numero invalido!\n");
-  }
-
-  char *Ms = "unidade";
-  char *Cs = "centena";
-  char *Ds = "dezena";
-  char *Us = "unidade";
-
-  if (m > 1)
-  {
-    Ms = "unidades";
-  }
-
-  if (c > 1)
-  {
-    Cs = "centenas";
-  }
-
-  if (d > 1)
-  {
-    Ds = "dezenas";
-  }
-
-  if (u > 1)
-  {
-    Us = "unidades";
-  }
-
-  char *o;
-
-  if (m > 0)
-  {
-    o = "quarta ordem";
-    printf("(%s) %d = ", o, n);
-    printf("%d %s de milhar + %d %s + %d %s + %d %s = %d + %d + %d + %d\n", m, Ms, c, Cs, d, Ds, u, Us, m * 1000, c * 100, d * 10, u);
-  }
-  else if (m == 0 && c > 0)
-  {
-    o = "terceira ordem";
-    printf("(%s) %d = ", o, n);
-    printf("%d %s + %d %s + %d %s = %d + %d + %d\n", c, Cs, d, Ds, u, Us, c * 100, d * 10, u);
-  }
-  else if (m == 0 && c == 0 && d > 0)
-  {
-    o = "segunda ordem";
-    printf("(%s) %d = ", o, n);
-    printf("%d %s + %d %s = %d + %d\n", d, Ds, u, Us, d * 10, u);
-  }
-  else if (m == 0 && c == 0 && d == 0 && u > 0)
-  {
-    o = "primeira ordem";
-    printf("(%s) %d = ", o, n);
-    printf("%d %s = %d\n", u, Us, u);
-  }
+        printf("= ");
+        switch(ordem){
+        case 4:
+                printf("%d ", m*1000);
+                if (c!=0){
+                    printf("+ %d ", c*100);
+                }
+                if( d!=0){
+                    printf("+ %d ", d*10);
+                }
+                if( u!=0){
+                    printf("+ %d", u);
+                }
+                printf("\n");
+                break;
+        case 3:	
+                printf("%d ", c*100);
+                if( d!=0){
+                    printf("+ %d ", d*10);
+                }
+                if( u!=0){
+                    printf("+ %d", u);
+                }
+                printf("\n");
+                break;
+        case 2:
+                printf("%d ", d*10);
+                if( u!=0){
+                    printf("+ %d", u);
+                }
+                printf("\n");
+                break;
+        case 1:
+                printf ("%d\n", u);		
+        }
+    }
 }
+
