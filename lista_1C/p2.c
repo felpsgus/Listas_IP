743b17dd345a1e1578c95318452d02cb320cae5a
Felipe Augusto <felipeaugustolopesveras@gmail.com>
2022-06-14 21:32:52 -0300

    Add files via upload

---
+++ lista_1C/p2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    double C, F;
+    int q;
+    int i = 0;
+    scanf("%d", &q);
+    while (i<q)
+    {
+        i++;
+        scanf("%lf",&F);
+        C = 5*(F-32)/9;
+        printf("%.2lf FAHRENHEIT EQUIVALE A %.2lf CELSIUS\n", F, C);
+    }
+    
+    /*double F[q];
+    for (int i = 0; i < q; i++)
+    {
+        scanf("%lf", &F[i]);
+    }
+    
+    for (int i = 0; i < q; i++)
+    {
+        C = 5*(F[i]-32)/9;
+        printf("%.2lf FAHRENHEIT EQUIVALE A %.2lf\n", F[i], C);
+    }*/
+}
