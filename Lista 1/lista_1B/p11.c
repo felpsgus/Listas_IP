d8daf286e858de14c53e422f46252f05e93eece7
Felipe Augusto <felipeaugustolopesveras@gmail.com>
2022-06-23 19:55:43 -0300

    Update p11.c

--- Lista 1/lista_1B/p11.c
+++ Lista 1/lista_1B/p11.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
 #include <math.h>
-
+ 
 int main()
 {
-  double a, b, c, x1, x2, delta;
-  scanf("%lf %lf %lf", &a, &b, &c);
-
-  delta = (b * b) - (4 * a * c);
-  x1 = (-b - sqrt(delta)) / (2 * a);
-  x2 = (-b + sqrt(delta)) / (2 * a);
+  float a, b, c, x1, x2, delta;
+  scanf("%f %f %f", &a, &b, &c);
+  delta = b * b - 4 * a * c;
+  x1 = (-b + sqrt(delta)) / (2 * a);
+  x2 = (-b - sqrt(delta)) / (2 * a);
   
   if (delta > 0)
   {
     printf("RAIZES DISTINTAS\n");
-    printf("X1 = %.2lf\n", x1);
-    printf("X2 = %.2lf\n", x2);
+    if (x1<x2)
+    {
+        printf("X1 = %.2f\n", x1);
+        printf("X2 = %.2f\n", x2);
+    } else 
+    {
+        printf("X1 = %.2f\n", x2);
+        printf("X2 = %.2f\n", x1);
+    }    
   }
-
   if (delta == 0)
   {
     printf("RAIZ UNICA\n");
-    printf("X1 = %.2lf\n", x1);
+    printf("X1 = %.2f\n", x1);
   }
-
   if (delta < 0)
   {
     printf("RAIZES IMAGINARIAS\n");
