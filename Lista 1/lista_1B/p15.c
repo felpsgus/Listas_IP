8b2fdf516a3a0d1a19a1e660ed2197e741da1022
Felipe Augusto <felipeaugustolopesveras@gmail.com>
2022-06-23 20:21:09 -0300

    Update p15.c

--- Lista 1/lista_1B/p15.c
+++ Lista 1/lista_1B/p15.c
@@ -5,18 +5,18 @@ int main()
   int numero, dm, m, c, d, u;
   scanf("%d", &numero);
 
+  dm = (numero - (numero % 10000)) / 10000;
+  m = ((numero % 10000) - (numero % 1000)) / 1000;
+  c = ((numero % 1000) - (numero % 100)) / 100;
+  d = ((numero % 100) - (numero % 10)) / 10;
+  u = numero % 10;
+
   if (numero > 99999)
   {
     printf("NUMERO INVALIDO\n");
   }
   else if (numero > 9999)
   {
-    dm = (numero - (numero % 10000)) / 10000;
-    m = ((numero % 10000) - (numero % 1000)) / 1000;
-    c = ((numero % 1000) - (numero % 100)) / 100;
-    d = ((numero % 100) - (numero % 10)) / 10;
-    u = numero % 10;
-
     if (dm == u && m == d)
     {
       printf("PALINDROMO\n");
@@ -28,11 +28,6 @@ int main()
   }
   else if (numero > 999)
   {
-    m = ((numero % 10000) - (numero % 1000)) / 1000;
-    c = ((numero % 1000) - (numero % 100)) / 100;
-    d = ((numero % 100) - (numero % 10)) / 10;
-    u = numero % 10;
-
     if (m == u && c == d)
     {
       printf("PALINDROMO\n");
@@ -44,10 +39,6 @@ int main()
   }
   else if (numero > 99)
   {
-    c = ((numero % 1000) - (numero % 100)) / 100;
-    d = ((numero % 100) - (numero % 10)) / 10;
-    u = numero % 10;
-
     if (c == u)
     {
       printf("PALINDROMO\n");
@@ -59,9 +50,6 @@ int main()
   }
   else if (numero > 9)
   {
-    d = ((numero % 100) - (numero % 10)) / 10;
-    u = numero % 10;
-
     if (d == u)
     {
       printf("PALINDROMO\n");
@@ -73,8 +61,6 @@ int main()
   }
   else if (numero > 0)
   {
-    u = numero % 10;
-
     if (u == 2 || u == 3 || u == 5 || u == 7)
     {
       printf("PALINDROMO\n");
