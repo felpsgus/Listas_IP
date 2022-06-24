5bc80a22be61deb4901bdf9250ec0d4a4fa7f311
Felipe Augusto <felipeaugustolopesveras@gmail.com>
2022-06-23 21:35:16 -0300

    Add files via upload

---
+++ Lista 1/lista_1C/p6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    double n, s, i;
+    scanf("%lf", &n);
+    s = 0;
+
+    if (n > 1)
+    {
+        for (i = 1; i <= n; i++)
+        {
+            s = s + 1/i;
+        }
+
+        printf("%.6lf\n", s);
+    }
+    else
+    {
+        printf("Numero invalido!");
+    }
+}
