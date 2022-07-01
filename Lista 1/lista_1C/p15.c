5d0bdbb890d000c76d25d8b10675fc5f75fca900
Felipe Augusto <felipeaugustolopesveras@gmail.com>
2022-06-30 21:49:30 -0300

    Update p15.c

--- Lista 1/lista_1C/p15.c
+++ Lista 1/lista_1C/p15.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdbool.h>
 #include <math.h>
 
 int main()
 {
   double n, e;
-  scanf("%lf", &n);
-  int rk, r = sqrtf(n);
+  scanf("%lf %lf", &n, &e);
+  double r = 1;
 
-  double limite = n - (rk ^ 2);
+  double limite = n - (r * r);
 
-  while (limite > e)
+  while ((n - (r * r)) > e)
   {
-    rk = ((rk - 1) + n / (rk - 1)) / 2;
-    printf("r: 1.500000000, erro: 0.250000000\n", rk);
+    r = ((r) + n / (r)) / 2;
+    printf("r: %.9lf, erro: %.9lf\n", r, e);
   }
 }
