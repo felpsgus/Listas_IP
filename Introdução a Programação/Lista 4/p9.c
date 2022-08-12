e2f2f94f1ee2ba23680369bb6bab3cf09f68dd5f
Felipe Augusto <felipeaugustolopesveras@gmail.com>
2022-08-11 21:37:42 -0300

    Add files via upload

---
+++ Introdução a Programação/Lista 4/p9.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int linhas, colunas, i, k, ver = 0, valor = 1;
+
+  scanf("%d", &linhas);
+  scanf("%d", &colunas);
+
+  int matriz[linhas][colunas];
+
+  for (i = 0; i < linhas; i++)
+  {
+    for (k = 0; k < colunas; k++)
+    {
+        if (ver == 0)
+        {
+            printf("0");
+            ver = 1;
+        }
+        else
+        {
+            printf("%d", valor);
+            valor++;
+            ver = 0;
+        }
+
+        if (ver == 0 && k == colunas - 1)
+        {
+            ver = 1;
+        }
+
+        if (k < colunas - 1)
+        {
+            printf(" ");
+        }
+    }
+    if (ver == 0 && k == colunas - 1)
+    {
+        ver = 1;
+    }
+    else
+    {
+        ver = 0;
+    }
+    printf("\n");
+  }
+}
